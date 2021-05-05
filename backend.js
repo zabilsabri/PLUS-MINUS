c226a211250e5d983989b9109e8e4870de9c0466
Zabil Sabri Muhammad <79674049+zabilsabri@users.noreply.github.com>
2021-05-05 19:20:34 +0800

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,18 +1,10 @@
 function buttonCounterPlus() {
-  if (localStorage.counter){
-    localStorage.counter = Number(localStorage.counter)+1
-  } else {
-    localStorage.counter = 1
-  }
+  localStorage.counter++
   document.getElementById("result").innerHTML = localStorage.counter
 }
 
 function buttonCounterMinus() {
-  if (localStorage.counter){
-    localStorage.counter = Number(localStorage.counter)-1
-  } else {
-    localStorage.counter = -1
-  }
+  localStorage.counter--
   document.getElementById("result").innerHTML = localStorage.counter
 }
 
